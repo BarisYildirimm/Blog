01c7339bf42a2039054f7f8d3ca48699517d97aa
BarisYildirimm <72316936+BarisYildirimm@users.noreply.github.com>
2021-09-17 23:25:48 +0300

    adminAdd

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
 
 router.get("/blog", (req, res) => {
-  res.render("admin/index");
+  res.render("admin/index", {
+    style: ".././css/home.css",
+  });
 });
+
+router.get("/blog/new", (req, res) => {
+    res.render("admin/index", {
+      style: ".././css/home.css",
+    });
+  });
+
 
 module.exports = router;
